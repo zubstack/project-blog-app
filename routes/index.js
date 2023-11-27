5f186a5a0b81ed82d4ea49e6a85be83c3653db2e
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-11-27 06:11:38 -0500

    [up] /login & user authentication

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const postsRouter = require("./posts.router.js");
 const usersRouter = require("./users.router.js");
+const loginRouter = require("./login.router.js");
 const express = require("express");
 
 function routerApi(app) {
   const router = express.Router();
   app.use("/api", router);
+  router.use("/login", loginRouter);
   router.use("/posts", postsRouter);
   router.use("/users", usersRouter);
 }
---
+++ routes/login.router.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../models/user");
+
+const router = express.Router();
+
+router.get("/", async (request, response) => {
+  const { username, password } = request.body;
+  const user = await User.findOne({ username });
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(password.toString(), user.passwordHash);
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({ error: "Invalid username or password" });
+  }
+
+  const userForToken = {
+    username: user.name,
+    id: user.id,
+  };
+
+  // eslint-disable-next-line no-undef
+  const token = jwt.sign(userForToken, process.env.SECRET);
+
+  response
+    .status(200)
+    .json({ username: user.username, name: user.name, token });
+});
+
+module.exports = router;
